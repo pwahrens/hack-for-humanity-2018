15499d04d096dbbd166941dc86e28bdc66dd873f
sjpacwa <stp0023@gmail.com>
2018-03-03 16:58:31 -0800

    Changed database
    Changed Lattitude and Longitude to location string

--- backend/h4h2018/server/models.py
+++ backend/h4h2018/server/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
-# Create your models here.
 
 class Message(models.Model):
     text = models.CharField(max_length=500)
     pub_date = models.DateTimeField('date published')
 
+
 class PhoneNumber(models.Model):
     text = models.CharField(max_length=30)
 
+
 class Location(models.Model):
-    Latitude = models.CharField(max_length=30)
-    Longitude = models.CharField(max_length=30)
+    location = models.CharField(max_length=100)
