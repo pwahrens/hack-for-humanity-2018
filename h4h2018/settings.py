97fd4a9477ff291d89cbd16ab769e14d24533072
sjpacwa <stp0023@gmail.com>
2018-03-04 04:25:43 -0800

    Settings changes

--- h4h2018/settings.py
+++ h4h2018/settings.py
@@ -125,8 +125,9 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 STATIC_URL = '/static/'
 
