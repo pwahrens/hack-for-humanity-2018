889ec9354d1d00bd368586911e684836f2d7ae78
Mason Bruce <mbruce@scu.edu>
2018-03-04 02:52:22 -0800

    filter stuff

--- front_end/scripts.js
+++ front_end/scripts.js
@@ -4,6 +4,7 @@ var geocoder;
 var markers = []
 var resources = {}
 var markerClusterer
+var types = {}
 
 resources['Food'] = 0
 resources['Water'] = 0
@@ -11,6 +12,13 @@ resources['Medicine'] = 0
 resources['Blankets'] = 0
 resources['Toiletries'] = 0
 resources['Power'] = 0
+
+types['Food'] = []
+types['Water'] = []
+types['Medicine'] = []
+types['Blankets'] = []
+types['Toiletries'] = []
+types['Power'] = []
 
 // Highcharts for the resource graph
 var chart = Highcharts.chart('resource-graph', {
@@ -115,7 +123,7 @@ function initMap() {
 
     for (var i = 0; i < checkboxes.length; ++i) {
         checkboxes[i].addEventListener('click', function() {
-            console.log(this.id)
+            toggleResources(this.id)
 
         })
     }
@@ -127,6 +135,14 @@ function initMap() {
         };
     });
 
+    var checkboxes = document.getElementsByClassName("resource-checkbox")
+
+    for (var i = 0; i < checkboxes.length; i++) {
+          checkboxes[i].addEventListener('click', function() {
+                      toggleResources(this.id)
+                  })
+    }
+
 }
 
 function refresh() {
@@ -136,6 +152,19 @@ function refresh() {
         apiCall();
 }
 
+
+function filter() {
+      refresh();
+      for (marker in markers) {
+            for (filter in filters) {
+                  for(var i in types[filters[filter]]) {
+                        if (types[filters[filter]][i] === marker) {
+                              removeMarker(marker)
+                        }
+                  }
+            }
+      }
+}
 
 
 function getCircle(magnitude) {
@@ -243,29 +272,137 @@ function httpGetAsync(theUrl, callback)
     xmlHttp.send(null);
 }
 
+var filters = []
+
+function toggleResources(resource) {
+    refresh()
+    var element = document.getElementById(resource + "-checkbox")
+    element.checked = !element.checked
+    switch(resource) {
+        case 'food':
+            console.log('food')
+            if (element.checked == false) {
+                  filters.push('Food')
+            } else {
+                  for (var i = 0; i < filters.length; i++) {
+                        if (filters[i] === 'Food') {
+                              if (i > -1) {
+                                  filters.splice(i, 1);
+                                  console.log("Removed");
+                              }
+                        }
+                  }
+            }
+            break;
+        case 'water':
+            console.log('water')
+            if (element.checked == false) {
+                  filters.push('Water')
+            } else {
+                  for (var i = 0; i < filters.length; i++) {
+                        if (filters[i] === 'Water') {
+                              if (i > -1) {
+                                  filters.splice(i, 1);
+                              }
+                        }
+                  }
+            }
+            break;
+        case 'medicine':
+            console.log('medicine')
+            if (element.checked == false) {
+                  filters.push('Medicine')
+            } else {
+                  for (var i = 0; i < filters.length; i++) {
+                        if (filters[i] === 'Medicine') {
+                              if (i > -1) {
+                                  filters.splice(i, 1);
+                              }
+                        }
+                  }
+            }
+            break;
+        case 'blankets':
+            console.log('blankets')
+            if (element.checked == false) {
+                  filters.push('Blankets')
+            } else {
+                  for (var i = 0; i < filters.length; i++) {
+                        if (filters[i] === 'Blankets') {
+                              if (i > -1) {
+                                  filters.splice(i, 1);
+                              }
+                        }
+                  }
+            }
+            break;
+        case 'toiletries':
+            console.log('toiletries')
+            if (element.checked == false) {
+                  filters.push('Toiletries')
+            } else {
+                  for (var i = 0; i < filters.length; i++) {
+                        if (filters[i] === 'Toiletries') {
+                              if (i > -1) {
+                                  filters.splice(i, 1);
+                              }
+                        }
+                  }
+            }
+            break;
+        case 'power':
+            console.log('power')
+            if (element.checked == false) {
+                  filters.push('Power')
+            } else {
+                  for (var i = 0; i < filters.length; i++) {
+                        if (filters[i] === 'Power') {
+                              if (i > -1) {
+                                  filters.splice(i, 1);
+                              }
+                        }
+                  }
+            }
+            break;
+        default:
+            console.log('default')
+    }
+
+      filter()
+}
+
 function apiCall() {
     var url = "http://7ecab5ef.ngrok.io"
     httpGetAsync(url + "/server/main/", function(response){
         var json = JSON.parse(response)
         for (var i in json.data) {
-            createMarker(json.data[i].location, "", null, json.data[i].text, json.data[i].phone_number)
+
+            var marker = createMarker(json.data[i].location, "", null, json.data[i].text, json.data[i].phone_number)
             if (json.data[i].food) {
                 ++resources['Food']
+
+                types['Food'].push(marker)
+
             }
             if (json.data[i].water) {
                 ++resources['Water']
+                types['Water'].push(marker)
             }
             if (json.data[i].medicine) {
                 ++resources['Medicine']
+                types['Medicine'].push(marker)
             }
             if (json.data[i].blankets) {
                 ++resources['Blankets']
+                types['Blankets'].push(marker)
             }
             if (json.data[i].toiletries) {
                 ++resources['Toiletries']
+                types['Toiletries'].push(marker)
             }
             if (json.data[i].power) {
                 ++resources['Power']
+                types['Power'].push(marker)
             }
         }
         chart.update({
