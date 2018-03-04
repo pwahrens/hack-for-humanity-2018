4b08d0930064bf11303bc291443addc075a59d46
sjpacwa <stp0023@gmail.com>
2018-03-03 19:47:40 -0800

    Database Changes

--- backend/h4h2018/server/database.py
+++ backend/h4h2018/server/database.py
@@ -1,21 +1,20 @@
 # Django imports
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from . import models
 
 
 @csrf_exempt
 def database(phone_number, text, location):
-    b = models.Message(phone_number=phone_number, text=text, location=location)
+    b = models.Message(phone_number=phone_number, text=text, location=location, food=False, water=False, medicine=False)
     b.save()
 
-
-    for msg in list(models.Message.objects.all()):
+    all_messages = list(models.Message.objects.all())
+    for msg in all_messages:
         print(msg)
 
     return HttpResponse("Working")
 
 
 def main_handle(request):
-    return HttpResponse(models.Message.objects.all())
-
+    return JsonResponse({'data': list(models.Message.objects.values()),})
--- backend/h4h2018/server/models.py
+++ backend/h4h2018/server/models.py
@@ -5,15 +5,9 @@ class Message(models.Model):
     phone_number = models.CharField(max_length=15)
     text = models.CharField(max_length=500)
     location = models.CharField(max_length=100)
+    food = models.BooleanField()
+    water = models.BooleanField()
+    medicine = models.BooleanField()
 
     def __str__(self):
         return "(" + self.phone_number + ", " + self.text + ", " + self.location + ")\n"
-
-
-
-#class PhoneNumber(models.Model):
-#    text = models.CharField(max_length=30)
-
-
-#class Location(models.Model):
-#    location = models.CharField(max_length=100)
