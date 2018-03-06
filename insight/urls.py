d066bc55342dd37e40d1c821bd4e6ca63a076b09
Paul Ahrens <pwahrens@gmail.com>
2018-03-05 23:11:11 -0800

    Update project names, remove Procfile, update .gitignore

--- insight/urls.py
+++ insight/urls.py
@@ -1,4 +1,4 @@
-"""h4h2018 URL Configuration
+"""insight URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
--- insight/wsgi.py
+++ insight/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for h4h2018 project.
+WSGI config for insight project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "h4h2018.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "insight.settings")
 
 application = get_wsgi_application()
